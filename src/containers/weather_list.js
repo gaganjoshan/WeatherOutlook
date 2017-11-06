5d6b39ca988404a969c88841c1edad2dd522de0f
Gagandeep Singh <gagandeep.singh@kp.org>
2017-11-06 13:24:18 -0800

    Add pressure and humidity charts and other improvements

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -13,9 +13,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>CITY</th>
-            <th>TEMPERATURE</th>
-            <th>PRESSURE</th>
-            <th>HUMIDITY</th>
+            <th>TEMPERATURE(K)</th>
+            <th>PRESSURE(Hpa)</th>
+            <th>HUMIDITY(%)</th>
           </tr>
         </thead>
         <tbody>
@@ -28,13 +28,15 @@ class WeatherList extends Component {
   renderWeather(cityData){
     const cityName = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
     console.log(temps);
     return(
       <tr key={cityName}>
         <td>{cityName}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        <td><Chart data={temps} color="orange" units = "K"/></td>
+        <td><Chart data={pressures} color="green" units = "Hpa"/></td>
+        <td><Chart data={humidities} color="black" units = "%"/></td>
       </tr>
     )
   }
